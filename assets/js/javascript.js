44289835dd4ab828ec7251aee18b4bca7186e389
karla nava <karla.nm8@gmail.com>
2017-03-23 09:07:10 +0000

    con botón eliminar

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,12 +1,28 @@
 function agregarFrutas() {
   var texto = document.getElementById("frutasIntroducidas").value;
-  var li = document.createElement("LI");
+  var li = document.createElement("li"); 
   
   li.innerHTML = texto == ''?'(fruta sin nombre)':texto;
   document.getElementById('lista').appendChild(li);
 }
 /*
   1. que el boton agrege el texto al arregloFrutas
+
   2. que el arreglo de frutas se llene según las frutas agregadas
   3. Imprimir frutas agregadas a la lista
 */
+
+function removeLi() {
+  var lista = document.getElementById("lista");
+  var elemento= document.getElementsByTagName("li");
+  var i = elemento.length -1;
+  lista.removeChild(elemento[i]);
+  }
+
+
+/*var texto = document.getElementById("frutasIntroducidas").value;
+var li = document.createElement("li");
+li.innerHTML = texto;
+
+document.getElementById('lista').appendChild(li);
+document.getElementById('lista').appendChild(li);*/
